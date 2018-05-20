a28eda1c6544ae65ffe7ed46c70c7db6926654b4
Stephen <steve.ward2@gmail.com>
2018-05-20 13:28:13 -0400

    added login

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
-// Web app
-var app = require('./app');
+'use strict';
 
-// Setting port for web app
-var port = 6194;
+/* Server */
+let express = require("express");
+let app = express();
 
-app.listen(3000, '0,0,0,0', () => {
-  console.log('Server started on port#' + port);
+/* Middleware */
+let fs = require('fs');
+let path = require('path');
+
+/* ROUTES */
+/* root */
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname + "/views/index.html"));
 });
+
+
+/* TO DO: We need to have a proper html file for our login page */
+app.get('/login', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname + "/views/login.html"));
+});
+
+
+
+/* No route found */
+app.get('*', (req, res) =>{
+    res.send("hmm, looks like we took a wrong term somewhere. Lets go back.");
+})
+
+
+
+/* Listen on port 3000 */
+app.listen(3000, ()=> {
+    console.log("Listening on port 3000");
+})
