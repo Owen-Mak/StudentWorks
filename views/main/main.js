f5517f08318524e8fde9fa877b9912984de9320f
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-06-04 13:12:22 -0400

    proper tile display with empty and accounts for null fields

--- views/main/main.js
+++ views/main/main.js
@@ -12,8 +12,8 @@ $(document).ready(() => {
         console.log("Cannot create an XMLHTTP instance");
 
     httpRequest.onreadystatechange = showContents;
-    httpRequest.open('GET', "http://myvmlab.senecacollege.ca:6193/api/getAllProjects", true);
-    //httpRequest.open('GET', "http://localhost:3000/api/getAllProjects", true);
+    //httpRequest.open('GET', "http://myvmlab.senecacollege.ca:6193/api/getAllProjects", true);
+    httpRequest.open('GET', "http://localhost:3000/api/getAllProjects", true);
 
     httpRequest.send();
 
@@ -33,34 +33,40 @@ function showContents() {
 
             // Language LIST ........................
             $.each(jsData, (key, value) => {
-                if (!unqArr.includes(value.language)) {
-                    unqArr[count++] = value.language;
-                    html += "<li href='#'>" + value.language + "</li>";
+                if (value.language) {
+                    if (!unqArr.includes(value.language)) {
+                        unqArr[count++] = value.language;
+                        html += "<li href='#'>" + value.language + "</li>";
+                    }
                 }
             });
             $("#lngList").append(html);
 
             // Framework LIST ......................
-            html= "";
+            html = "";
             cout = 0;
             unqArr = []
             $.each(jsData, (key, value) => {
-                if (!unqArr.includes(value.framework)) {
-                    unqArr[count++] = value.framework;
-                    html += "<li href='#'>" + value.framework + "</li>";
+                if (value.framework) { // checks for null
+                    if (!unqArr.includes(value.framework)) {
+                        unqArr[count++] = value.framework;
+                        html += "<li href='#'>" + value.framework + "</li>";
+                    }
                 }
             });
             $("#frmList").append(html);
 
             // Year LIST ......................
-            html= "";
+            html = "";
             cout = 0;
             unqArr = []
             $.each(jsData, (key, value) => {
-                var year = value.creationDate.substring(0, 4);
-                if (!unqArr.includes(year)) {
-                    unqArr[count++] = year;
-                    html += "<li href='#'>" + year + "</li>";
+                if (value.creationDate) {
+                    var year = value.creationDate.substring(0, 4);
+                    if (!unqArr.includes(year)) {
+                        unqArr[count++] = year;
+                        html += "<li href='#'>" + year + "</li>";
+                    }
                 }
             });
             $("#yearList").append(html);
@@ -69,31 +75,48 @@ function showContents() {
             // BODY TILES ------------------------
             var projectCount = Object.keys(jsData).length; // Assuming 6 or less at the moment
             var count = 1;
-            $.each(jsData, (key, value) =>{
-                var title = value.title;
-                var image = value.ImageFilePath; 
-
-                var year = value.creationDate.substring(0, 4);
-                var framework = value.framework;
-                var language = value.language;
+            $.each(jsData, (key, value) => {
                 var html = "";
+                var title = value.title;
+                var year = value.creationDate ? value.creationDate.substring(0, 4) : "";
+                var image = value.ImageFilePath;
+                var language = value.language;
+                var framework = value.framework;
 
-                var finalIm = '<img src="' + image + '"';
-                finalIm += ' class="img-responsive center-block" alt="icon" >';
-                console.log(finalIm);
+                html += renderTile(title, year, image, language, framework);
 
-                html += "<div class='col-md-4'>";
-                html += "<div class='panel panel-default' style='width:360px;' >";
-                html += "   <div class='panel-heading'><h4>" + title + " ( " +year +" )</h4></div>";
-                html += "   <div class='panel-body' style='height:270px; '>" + finalIm + "</div>";
-                html += "   <div class='panel-footer'> <b>Language: </b>" + language
-                 + ", <b>Framework: </b> " + framework + "</div>"
-                html += "</div>";
-
-                count++;
                 $("#mainBody").append(html);
             });
             //------------------------------
         }
+    }
+
+    function renderTile(title, year, icon, language, framework) {
+        var imageShow = '<img src="' + icon + '" class="img-responsive center-block" alt="icon" >';
+        var titleShow = (title == "empty") ? "Future Project" : title + " [ <b>" + year + "</b> ] ";
+        var footer;
+
+        if (language || framework) {
+            var languageShow = (language) ? "<b>Language: </b>" + language : "";
+            var frameworkShow = (framework) ? ", <b>Framework: </b> " + framework : "&nbsp;";
+            footer = languageShow + frameworkShow;
+        } else {
+            footer = "<div style='text-align: center;'><a href='#' >Contribute</a></div>";
+        }
+
+
+        html = "";
+        html += "<div class='col-md-4'>";
+        html += "<div class='panel panel-default' style='width:360px;' >";
+        html += "   <div class='panel-heading' style='text-align: center;'><h4>" + titleShow + "</h4></div>";
+        html += "   <div class='panel-body' style='height:270px; '>" + imageShow + "</div>";
+        html += "   <div class='panel-footer' style='text-align: right;'> " + footer + "</div>";
+        html += "</div>";
+
+        return html;
+    }
+
+    function renderTileEmpty() {
+
     }
 }
