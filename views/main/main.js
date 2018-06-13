a11f702948e49183aa03057c7c8ef7ba8600ab91
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-06-13 13:21:04 -0400

    additional refactoring, adding comments, setting proper variable scope

--- views/main/main.js
+++ views/main/main.js
@@ -3,7 +3,6 @@ var httpRequest;
 var start;
 var prjNum;
 var lastPAge;
-var remainder;
 
 // READY function that gets called when HTML finishes parsing
 // console.log are used for deguggin and can be seen from BROWSER console not server.
@@ -19,7 +18,6 @@ $(document).ready(() => {
     httpRequest.send();
 });
 
-
 function showContents() {
     if (httpRequest.readyState === 4) {
         if (httpRequest.status === 200) {
@@ -27,10 +25,11 @@ function showContents() {
 
             prjNum = jsData.length;
             lastPage = false;
-            var html = "";
+
             let languageList = "";
             let frameworkList = "";
             let yearList = "";
+            let tileNav = "";
 
             let languageArr = [];
             let frameworkArr = [];
@@ -59,13 +58,11 @@ function showContents() {
                         yearList += "<li href='#'>" + year + "</li>";
                     }
                 }
-
-
             });
 
             $("#lngList").append(languageList);
             $("#frmList").append(frameworkList);
-            //$("#yearList").append(yearList);
+            $("#yearList").append(yearList);
 
 
             // BODY TILES ------------------------
@@ -73,13 +70,13 @@ function showContents() {
             renderSixProjectTiles(jsData);
 
             // TILE NAViGATION --------------------
-            html = "";
-            html += '<button type="button" class="btn btn-default" id="prevBtn" aria-label="Left Align">'; // PREVIOUS Button
-            html += '<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></button>';
+            tileNav = "";
+            tileNav += '<button type="button" class="btn btn-default" id="prevBtn" aria-label="Left Align">'; // PREVIOUS Button
+            tileNav += '<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></button>';
 
-            html += '<button type="button" class="btn btn-default" id="nextBtn" aria-label="Right Align">'; // NEXT Button
-            html += '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></button>';
-            $("#tileNav").append(html);
+            tileNav += '<button type="button" class="btn btn-default" id="nextBtn" aria-label="Right Align">'; // NEXT Button
+            tileNav += '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></button>';
+            $("#tileNav").append(tileNav);
 
 
             $("#prevBtn").click(() => {
@@ -109,70 +106,69 @@ function renderSixProjectTiles(jsData) {
 
     // Main loop for six projects
     for (var projects = 0; projects < 6; projects++) {
-        if (start > (jsData.length - 1)) {
-            // render empty tile
-            html = renderEmptyTile();
-            $("#mainBody").append(html);
-            lastPage = true;
-            remainder++;
-            start++;
-        } else {//
-            lastPage = false;
-            var html = "";
-            var title = jsData[start].title;
-            var year = jsData[start].creationDate ? jsData[start].creationDate.substring(0, 4) : "";
-            var image = jsData[start].ImageFilePath;
-            var language = jsData[start].language;
-            var framework = jsData[start].framework;
-            var id = jsData[start].projectID;
 
-            html += renderTile(title, year, image, language, framework, id);
-            $("#mainBody").append(html);
+        if (start <= (jsData.length - 1)) { // Render PROJECT tile
+            
+            var title     = jsData[start].title;
+            var year      = jsData[start].creationDate ? jsData[start].creationDate.substring(0, 4) : "";
+            var image     = jsData[start].ImageFilePath;
+            var language  = jsData[start].language;
+            var framework = jsData[start].framework;
+            var id        = jsData[start].projectID;
+
+            var prjHtml = renderTile(title, year, image, language, framework, id);
+            $("#mainBody").append(prjHtml);
+
+            start++;
+            lastPage = false;
+        } 
+        else { // Render Empty tile
+            var prjHtml  = renderEmptyTile();
+            $("#mainBody").append(prjHtml);
+
+            lastPage = true;
             start++;
         }
-
     }
-
-    return html;
 }
 
 // Renders single tile
 function renderTile(title, year, icon, language, framework, id) {
-    var imageShow = '<img src="' + icon + '" class="img-responsive center-block;" style="height: 220px; margin:auto;" alt="icon" >';
-    var titleShow = title + " (<strong>" + year + "</strong>) ";
-    var languageShow = (language) ? "<b>Language: </b>" + language : "";
-    var frameworkShow = (framework) ? ", <b>Framework: </b> " + framework : "&nbsp;";
-    var footer = languageShow + frameworkShow;
+    let imageShow = '<img src="' + icon + '" class="img-responsive center-block;" style="height: 220px; margin:auto;" alt="icon" >';
+    let titleShow = title + " (<strong>" + year + "</strong>) ";
+    let languageShow = (language) ? "<b>Language: </b>" + language : "";
+    let frameworkShow = (framework) ? ", <b>Framework: </b> " + framework : "&nbsp;";
+    let footer = languageShow + frameworkShow;
 
-    html = "";
-    html += "<div class='col-md-4'>";
-    html += "<div class='panel panel-default' style='width:360px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);' >";
-    html += "   <div class='panel-heading' style='text-align: center;'><h4>" + titleShow + "</h4></div>";
-    html += "       <a href='#' id='prjLink" + id + "' class ='tileLink' style='text-decoration: none;' onclick='readyProject(" + id + ")'>";
-    html += "          <div class='panel-body' style='height:250px; '>" + imageShow + "</div>";
-    html += "       </a>";
-    html += "   <div class='panel-footer' style='text-align: right;'> " + footer + "</div>";
-    html += "</div>";
+    let tileHtml = "";
+    tileHtml += "<div class='col-md-4'>";
+    tileHtml += "<div class='panel panel-default' style='width:360px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);' >";
+    tileHtml += "   <div class='panel-heading' style='text-align: center;'><h4>" + titleShow + "</h4></div>";
+    tileHtml += "       <a href='#' id='prjLink" + id + "' class ='tileLink' style='text-decoration: none;' onclick='readyProject(" + id + ")'>";
+    tileHtml += "          <div class='panel-body' style='height:250px; '>" + imageShow + "</div>";
+    tileHtml += "       </a>";
+    tileHtml += "   <div class='panel-footer' style='text-align: right;'> " + footer + "</div>";
+    tileHtml += "</div>";
 
-    return html;
+    return tileHtml;
 }
 
 // Renders empty tile
 function renderEmptyTile() {
-    var imageShow = '<img src="images/empty.png" class="img-responsive center-block;" style="height: 220px; margin:auto;" alt="icon" >';
-    var footer = "<div style='text-align: center;'><a href='#' >Contribute</a></div>";
+    let image = '<img src="images/empty.png" class="img-responsive center-block;" style="height: 220px; margin:auto;" alt="icon" >';
+    let footer = "<div style='text-align: center;'><a href='#' >Contribute</a></div>";
 
-    html = "";
-    html += "<div class='col-md-4'>";
-    html += "<div class='panel panel-default' style='width:360px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);' >";
-    html += "   <div class='panel-heading' style='text-align: center;'><h4>Future Proejct</h4></div>";
-    html += "       <a href='#' class ='tileLinkEmpty' style='text-decoration: none;'>";
-    html += "          <div class='panel-body' style='height:250px; '>" + imageShow + "</div>";
-    html += "       </a>";
-    html += "   <div class='panel-footer' style='text-align: right;'> " + footer + "</div>";
-    html += "</div>";
+    let emptyTileHtml = "";
+    emptyTileHtml += "<div class='col-md-4'>";
+    emptyTileHtml += "<div class='panel panel-default' style='width:360px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);' >";
+    emptyTileHtml += "   <div class='panel-heading' style='text-align: center;'><h4>Future Proejct</h4></div>";
+    emptyTileHtml += "       <a href='#' class ='tileLinkEmpty' style='text-decoration: none;'>";
+    emptyTileHtml += "          <div class='panel-body' style='height:250px; '>" + image + "</div>";
+    emptyTileHtml += "       </a>";
+    emptyTileHtml += "   <div class='panel-footer' style='text-align: right;'> " + footer + "</div>";
+    emptyTileHtml += "</div>";
 
-    return html;
+    return emptyTileHtml;
 }
 
 
@@ -199,35 +195,34 @@ function showProject() {
             var jsData = JSON.parse(httpRequest.responseText);
 
             if (jsData[0].title != "empty") {
-                var year = jsData[0].creationDate ? jsData[0].creationDate.substring(0, 4) : "";
-                var videoLink = jsData[0].VideoUrl;
-                var languageShow = (jsData[0].language) ? "<p><b>Language: </b>" + jsData[0].language + "</p>" : "";
-                var frameworkShow = (jsData[0].framework) ? "<p><b>Framework: </b> " + jsData[0].framework + "</p>" : "";
-                var desc = (jsData[0].description) ? "<p>" + jsData[0].description + "</p>" : "";
-                var html = "";
+                let year = jsData[0].creationDate ? jsData[0].creationDate.substring(0, 4) : "";
+                let videoLink = jsData[0].VideoUrl;
+                let languageShow = (jsData[0].language) ? "<p><b>Language: </b>" + jsData[0].language + "</p>" : "";
+                let frameworkShow = (jsData[0].framework) ? "<p><b>Framework: </b> " + jsData[0].framework + "</p>" : "";
+                let desc = (jsData[0].description) ? "<p>" + jsData[0].description + "</p>" : "";
 
                 // Title
-                html += "<h2 style='text-align: center;'>" + jsData[0].title + " [ " + year + " ] </h2><br>";
+                let prjHtml = "<h2 style='text-align: center;'>" + jsData[0].title + " [ " + year + " ] </h2><br>";
 
                 // Video and Info
-                html += "<div class='container'>";
-                html += "   <div class='row'>";
-                html += "      <div class='col-md-8' id='videoCol'>";
-                html += "         <div class='embed-responsive embed-responsive-16by9'>";
-                html += "            <iframe class='embed-responsive-item' id='prjVideo' src='" + videoLink + "'></iframe>";
-                html += "         </div>";
-                html += "      </div>";
-                html += "      <div class='col-md-4' id='infoCol'>";
-                html += "         <br><h4> Contributors:</h4> <p>Vasia Jopovych</p><p>Vaselisa Pizdaivanovna</p><p>Johnny Waters</p>";
-                html += "         <br><h4 id='prjTitle'> Project info:</h4>" + languageShow + frameworkShow;
-                html += "      </div>";
-                html += "   </div>";
-                html += "   <div class='row'>";
-                html += "      <h3>Description</h3>" + desc;
-                html += "   </div>";
-                html += "</div>";
+                prjHtml += "<div class='container'>";
+                prjHtml += "   <div class='row'>";
+                prjHtml += "      <div class='col-md-8' id='videoCol'>";
+                prjHtml += "         <div class='embed-responsive embed-responsive-16by9'>";
+                prjHtml += "            <iframe class='embed-responsive-item' id='prjVideo' src='" + videoLink + "'></iframe>";
+                prjHtml += "         </div>";
+                prjHtml += "      </div>";
+                prjHtml += "      <div class='col-md-4' id='infoCol'>";
+                prjHtml += "         <br><h4> Contributors:</h4> <p>Vasia Jopovych</p><p>Vaselisa Pizdaivanovna</p><p>Johnny Waters</p>";
+                prjHtml += "         <br><h4 id='prjTitle'> Project info:</h4>" + languageShow + frameworkShow;
+                prjHtml += "      </div>";
+                prjHtml += "   </div>";
+                prjHtml += "   <div class='row'>";
+                prjHtml += "      <h3>Description</h3>" + desc;
+                prjHtml += "   </div>";
+                prjHtml += "</div>";
 
-                $("#mainBody").html(html);
+                $("#mainBody").html(prjHtml);
                 $("#tileNav").empty();
             }
         }
