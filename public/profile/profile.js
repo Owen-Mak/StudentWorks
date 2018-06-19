de74961deee9cba7eeb811e4fb1b158a873ee59f
Huda Al Dallal <huda_wadi@hotmail.com>
2018-06-19 19:01:55 -0400

    Added profile, registration and login

--- public/profile/profile.js
+++ public/profile/profile.js
@@ -1,3 +1,47 @@
+
+$(document).ready(() => {
+  httpRequest = new XMLHttpRequest();
+  if (!httpRequest)
+      console.log("Cannot create an XMLHTTP instance");
+
+      console.log("IM LOADED");
+
+  id = 2;
+  httpRequest.onreadystatechange = renderProfile;
+  //httpRequest.open('GET', "http://myvmlab.senecacollege.ca:6193/api/getOneProject?id="+id, true);
+  httpRequest.open('GET', "http://localhost:3000/api/getProjectsByUser/UserID/" + id, true);
+  httpRequest.send();
+});
+
+function renderProfile() {
+  if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+
+        console.log("I got something");
+
+          var jsData = JSON.parse(httpRequest.responseText);
+
+          let projectList = "";
+          let projectStatusList = "";
+          let projectYearList = "";
+
+            projectList += "<li>"+ jsData[0].title + "</li>";
+            projectStatusList += "<li>NO STATUS IN OUR MODEL</li>";
+            projectYearList += "<li>"+ jsData[0].creationDate.substring(0, 4) + "</li>";
+
+            console.log(projectList);
+            console.log(projectStatusList);
+            console.log(projectYearList);
+
+
+          $("#projectName").html(projectList);
+          $("#projectStatus").html(projectStatusList);
+          $("#projectYear").html(projectYearList);
+      }
+  }
+}
+
+
 $(function() {
     $('#img-preview').click(function() {
       $('#img-input').trigger('click');
@@ -20,3 +64,4 @@ $(function() {
       }
     };
   });
+
