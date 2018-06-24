8867c01a1199a2746dcf11253b8d423b5478a7e6
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-06-24 15:14:25 -0400

    changing API routes to production for PR

--- public/contribute/contribute.js
+++ public/contribute/contribute.js
@@ -1,16 +1,5 @@
 $(document).ready(() => {
     userID = $("#userID").text();
-    let url = "";
-
-    // LOCAL
-    url = "http://localhost:3000/api/getOneProject/?id=" + userID;   
-
-    // PRODUCTION
-    //url = "http://myvmlab.senecacollege.ca:6193/api/getOneProject/?id=" + userID;   
-
-    $.getJSON(url, (data) => {
-        console.log(data);
-    });
 
     renderUserMenu();
 
