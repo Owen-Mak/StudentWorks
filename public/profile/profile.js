51fe71480afa79cc74ad700ac7bff2b0c2970e58
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-06-24 01:46:34 -0400

    Updating project.js, profile.js to render based on user setting

--- public/profile/profile.js
+++ public/profile/profile.js
@@ -1,39 +1,55 @@
-
 $(document).ready(() => {
-  httpRequest = new XMLHttpRequest();
-  if (!httpRequest)
-    console.log("Cannot create an XMLHTTP instance");
+  if ($("#userType").text() === "Visitor"){
+    renderUserMenu(); // so they can log in
+    return;
+  } 
 
-  id = 2;
-  httpRequest.onreadystatechange = renderProfile;
-  //httpRequest.open('GET', "http://myvmlab.senecacollege.ca:6193/api/getProjectsByUser/UserID/" + 2, true);
-  httpRequest.open('GET', "http://localhost:3000/api/getProjectsByUser/UserID/" + id, true);
-  httpRequest.send();
+  let id = $("#userID").text();
+
+  // LOCAL
+  //let prjUrl = "http://localhost:3000/api/getProjectsByUser/UserID/" + id;
+  //let usrUrl = "http://localhost:3000/api/getUserByID?id=" + id;
+
+  // PRODUCTION
+  let prjUrl = "http://myvmlab.senecacollege.ca:6193/api/getProjectsByUser/UserID/" + id;
+  let usrUrl = "http://myvmlab.senecacollege.ca:6193/getUserByID/" + id;
+
+  $.getJSON(prjUrl, (jsData) => { renderProjectList(jsData); });
+  $.getJSON(usrUrl, (jsData) => { renderUserDetails(jsData); });
 });
 
-function renderProfile() {
-  if (httpRequest.readyState === 4) {
-    if (httpRequest.status === 200) {
 
-      var jsData = JSON.parse(httpRequest.responseText);
-      console.log(jsData);
-      let projectList = "";
-      let projectStatusList = "";
-      let projectYearList = "";
-      $.each(jsData, (key, value) => {
+function renderProjectList(jsData) {
+  let projectList = "";
+  let projectStatusList = "";
+  let projectYearList = "";
 
-        projectList += "<li>" + value.title + "</li>";
-        projectStatusList += "<li>" + value.status + "</li>";
-        projectYearList += "<li>" + value.creationDate.substring(0, 4) + "</li>";
-      });
+  $.each(jsData, (key, value) => {
+    projectList += "<li>" + value.title + "</li>";
+    projectStatusList += "<li>" + value.status + "</li>";
+    projectYearList += "<li>" + value.creationDate.substring(0, 4) + "</li>";
+  });
 
-      renderUserMenu(); // function declared in usermenue.js
-      $("#pageTitleID").html("Profile");
-      $("#projectName").html(projectList);
-      $("#projectStatus").html(projectStatusList);
-      $("#projectYear").html(projectYearList);
-    }
-  }
+  renderUserMenu(); // function declared in usermenue.js
+  $("#pageTitleID").html("Your Profile");
+  $("#projectName").html(projectList);
+  $("#projectStatus").html(projectStatusList);
+  $("#projectYear").html(projectYearList);
+}
+
+function renderUserDetails(jsData) {
+  let fName = jsData[0].firstName ? jsData[0].firstName : "First Name";
+  let lName = jsData[0].lastName ? jsData[0].lastName : "Last Name";
+  let email = jsData[0].email ? jsData[0].email : "sample@email.com";
+  let program = jsData[0].program ? jsData[0].program : "Program of study";
+  let username = jsData[0].username ? jsData[0].username : "Username";
+
+
+  $("#fname").attr({ "placeholder": fName });
+  $("#lname").attr({ "placeholder": lName });
+  $("#email").attr({ "placeholder": email });
+  $("#program").attr({ "placeholder": program });
+  $("#username").attr({ "placeholder": username });
 }
 
 
