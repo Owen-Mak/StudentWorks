c4901d43f13e88210d11348221b7ef8079a1b06a
Yuriy Kartuzov <kyuriy@myseneca.ca>
2018-06-24 13:57:37 -0400

    Starting point. Setting api routes to local env

--- public/contribute/contribute.js
+++ public/contribute/contribute.js
@@ -2,7 +2,10 @@ $(document).ready(() => {
     userID = $("#userID").text();
     let url = "";
 
+    // LOCAL
     url = "http://localhost:3000/api/getOneProject/?id=" + userID;   
+
+    // PRODUCTION
     //url = "http://myvmlab.senecacollege.ca:6193/api/getOneProject/?id=" + userID;   
 
     $.getJSON(url, (data) => {
