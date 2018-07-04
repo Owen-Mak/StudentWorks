12dac2384105784343e5e2714699b59044c79a92
Stephen <steve.ward2@gmail.com>
2018-07-04 16:15:21 -0400

    program of study had NULL as placeholder, changed the placeholder to perform as the others do. Production connections added back in as well

--- public/profile/profile.js
+++ public/profile/profile.js
@@ -121,7 +121,7 @@ function renderUserDetails(jsData) {
   let fName = (jsData[0].firstName && jsData[0].firstName != "NULL") ? jsData[0].firstName : "First Name";
   let lName = (jsData[0].lastName && jsData[0].lastName != "NULL") ? jsData[0].lastName : "Last Name";
   let email = jsData[0].email ? jsData[0].email : "sample@email.com";
-  let program = jsData[0].program ? jsData[0].program : "Program of study";
+  let program = jsData[0].program != "NULL" ? jsData[0].program : "Program of study";
   let username = jsData[0].userName ? jsData[0].userName : "Username";
   let description = jsData[0].userDescription ? jsData[0].userDescription : "Description";
   let host = "http://myvmlab.senecacollege.ca:6193";
