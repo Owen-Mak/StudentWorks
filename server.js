33037d8c95466d0de9311084819662898716ac85
stephen <steve.ward2@gmail.com>
2018-07-26 17:58:55 -0400

    Added commenting to project page. Uses MongoDB, connection can be found in projectPage/comments.js. Comments are shown based on the projectID of the url. Each page has it's own comments. User's can comment, if no one is logged in, the username is anonymous. Functionality to delete will be added later

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const nodemailer = require("nodemailer");
 const app = express();
 const auth = require('./auth');
+const commentDB = require('./public/projectPage/comments.js')
 const dbconnect = require('./db_connect');
 const path = require("path");
 const multer = require('multer');
 const exphbs = require('express-handlebars');
+const queryString = require('query-string');
 let Client = require('ssh2-sftp-client');
 let sftp = new Client();
 
@@ -269,13 +271,48 @@ app.get("/", (req, res) => {
 });
 
 //PROJECT page
-app.get('/projectPage', (req, res) => {
-    res.status(200).render('project', {
-        authenticate: req.session.authenticate,
-        userID: req.session.userID,
-        userType: req.session.userType
-    });
+app.get('/projectPage', urlencodedParser, (req, res) => {
+    console.log(req.query.id);
+    commentDB.initialize(req.query.id)
+             .then(commentDB.getAllComments, null)
+             .then((commentsFromDB)=>{
+                res.status(200).render('project', {
+                    authenticate: req.session.authenticate,
+                    userID: req.session.userID,
+                    userType: req.session.userType,
+                    comments: commentsFromDB
+                });
+            })
+            .catch((error)=>{
+                console.log("inside of NO DB CONNECTION");
+                //don't give em comments if the DB doesn't connect.
+                res.status(200).render('project', {
+                    authenticate: req.session.authenticate,
+                    userID: req.session.userID,
+                    userType: req.session.userType
+                });
+            })
+
+            
 });
+//ADDING COMMENTS TO PROFILE PAGE
+app.post('/addComment',  urlencodedParser, (req, res) =>{
+   
+    var comment = {
+        projectID:   req.body.projectID, 
+        authorName:  req.session.userName ? req.session.userName : "anonymouse",
+        commentText: req.body.commentText
+    }
+    commentDB.addComment(comment).then(() => {
+        res.redirect("/");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/");
+        });
+
+});
+    
 
 //PROFILE page
 app.get('/profile', (req, res) => {
@@ -304,7 +341,7 @@ app.get('/contribute', (req,res) => {
 });
 
 //RECORDING page + Upload Video
-app.get('/recording', (req,res) => {
+app.get('/recording', ensureLogin, (req,res) => {
     res.sendFile(path.join(__dirname, 'public/recording/recording.html'));                                  
 });
 
@@ -313,6 +350,7 @@ app.post('/upload-video', uploadVideo.single('video-blob'), (req, res, next) => 
     //turn video path into readable path on VM
     var changed = file.replace(/\\/g, '/');
     //send back the video path
+    changed = "/" + changed;
     res.status(200).send(changed);
     
 });
@@ -1100,8 +1138,8 @@ app.use(function (req, res) {
 
 //Get a proper port
 var port = process.env.PORT || 3000;
-const server = https.createServer(sslOptions, app).listen(port, () => {
-    console.log("Express Listening on port: " + port);
-});
+    const server = https.createServer(sslOptions, app).listen(port, () => {
+        console.log("Express Listening on port: " + port);
+})
 
 
