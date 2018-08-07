f106cc082716874febba70723d126a0fd48c33cd
unknown <omak@myseneca.ca>
2018-08-06 22:50:43 -0400

    updated database scripts

---
+++ sqlScripts/DeleteTables.sql
@@ -0,0 +1,12 @@
+/*
+Group 7 - StudentWorks
+Last Modified - August 6th, 2018
+
+The following script deletes the four tables in mysql server for StudentWorks project
+*/
+use sw;
+
+drop table BRIDGE_USERS_PROJECTS;
+drop table PROJECTS;
+drop table COMMENTS;
+drop table USERS;
